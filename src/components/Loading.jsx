52a00dc23d424dfb609198fd5a4c73cbd9e7b082
Fahri Akbaba <96592272+fahriakbaba@users.noreply.github.com>
2022-08-21 09:42:33 +0300

    update Loading.jsx

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Loading() {
   return (
     <section style={{minHeight:"100vh"}} className="d-flex justify-content-center align-items-center">
-      <div class="loader"></div>
+      <div className="loader"></div>
       <span className="ms-2 fs-1 f">Loading...</span>
     </section>
   );
